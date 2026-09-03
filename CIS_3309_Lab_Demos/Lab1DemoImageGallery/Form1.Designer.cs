54f6a31b61dd2a16d8bd736c7b5bda503d4a9405
Korey Rodi <rodikorey@gmail.com>
2026-09-03 16:43:04 -0400

    9/3

---
+++ CIS_3309_Lab_Demos/Lab1DemoImageGallery/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Lab1DemoImageGallery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNextForm = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNextForm
+            // 
+            this.btnNextForm.Location = new System.Drawing.Point(32, 330);
+            this.btnNextForm.Name = "btnNextForm";
+            this.btnNextForm.Size = new System.Drawing.Size(501, 82);
+            this.btnNextForm.TabIndex = 0;
+            this.btnNextForm.Text = "Click here to take a PHLY Sports survey";
+            this.btnNextForm.UseVisualStyleBackColor = true;
+            this.btnNextForm.Click += new System.EventHandler(this.btnNextForm_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(357, 282);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(148, 37);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next Image";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(53, 282);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(151, 36);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Last Image";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::Lab1DemoImageGallery.Properties.Resources._7;
+            this.pictureBox1.Location = new System.Drawing.Point(151, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblName.Location = new System.Drawing.Point(210, 236);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(144, 26);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Player Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(578, 444);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnNextForm);
+            this.Name = "Form1";
+            this.Text = "Image Gallery";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNextForm;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ CIS_3309_Lab_Demos/Lab1DemoImageGallery/Form2.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Lab1DemoImageGallery
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.cboFavAthlete = new System.Windows.Forms.ComboBox();
+            this.lstFavoriteTeam = new System.Windows.Forms.CheckedListBox();
+            this.radMailingList = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Algerian", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(48, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(605, 45);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Take My PHLY Sports Survey";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.BackColor = System.Drawing.Color.White;
+            this.txtFullName.Location = new System.Drawing.Point(66, 75);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(577, 26);
+            this.txtFullName.TabIndex = 1;
+            this.txtFullName.Text = "Full Name";
+            // 
+            // cboFavAthlete
+            // 
+            this.cboFavAthlete.FormattingEnabled = true;
+            this.cboFavAthlete.Items.AddRange(new object[] {
+            "Jalen Brown",
+            "Bryce Harper",
+            "Chase Utley",
+            "Jalen Hurts",
+            "Lebron James"});
+            this.cboFavAthlete.Location = new System.Drawing.Point(187, 107);
+            this.cboFavAthlete.Name = "cboFavAthlete";
+            this.cboFavAthlete.Size = new System.Drawing.Size(317, 28);
+            this.cboFavAthlete.TabIndex = 2;
+            this.cboFavAthlete.Text = "Who is your Favoite PHLY Athlete";
+            // 
+            // lstFavoriteTeam
+            // 
+            this.lstFavoriteTeam.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lstFavoriteTeam.FormattingEnabled = true;
+            this.lstFavoriteTeam.Items.AddRange(new object[] {
+            "PHILLIES",
+            "EAGLES",
+            "UNION",
+            "FLYERS",
+            "76\'ers"});
+            this.lstFavoriteTeam.Location = new System.Drawing.Point(205, 150);
+            this.lstFavoriteTeam.Name = "lstFavoriteTeam";
+            this.lstFavoriteTeam.Size = new System.Drawing.Size(265, 119);
+            this.lstFavoriteTeam.TabIndex = 3;
+            // 
+            // radMailingList
+            // 
+            this.radMailingList.AutoSize = true;
+            this.radMailingList.Location = new System.Drawing.Point(227, 275);
+            this.radMailingList.Name = "radMailingList";
+            this.radMailingList.Size = new System.Drawing.Size(212, 24);
+            this.radMailingList.TabIndex = 4;
+            this.radMailingList.TabStop = true;
+            this.radMailingList.Text = "Select to Join Mailing List";
+            this.radMailingList.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(678, 644);
+            this.Controls.Add(this.radMailingList);
+            this.Controls.Add(this.lstFavoriteTeam);
+            this.Controls.Add(this.cboFavAthlete);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form2";
+            this.Text = "PHLY Sports Survey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.ComboBox cboFavAthlete;
+        private System.Windows.Forms.CheckedListBox lstFavoriteTeam;
+        private System.Windows.Forms.RadioButton radMailingList;
+    }
+}
