98de20274c97f8b6e972f4a286929aeeecbfd139
Korey Rodi <rodikorey@gmail.com>
2026-09-16 18:40:17 -0400

    9/16

---
+++ Lab 2/SandwichBuilder/SandwichBuilder/Customer.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace SandwhichBuilder
+{
+    internal class Customer
+    {
+        String name;
+        String address;
+        String phoneNumber;
+
+        public String Name
+        {
+            get {return name; } set {name = value; }
+        }
+        public String Address
+        {
+            get {return address; } set {address = value; }
+        }
+        public String PhoneNumber
+        {
+            get {return phoneNumber; } set{phoneNumber = value;}
+        }
+
+        internal Customer(String name, String address, String phoneNumber)
+        {
+            this.name = name;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+        public override string ToString()
+        {
+            String customerInfo = "";
+
+            customerInfo += name + "\n";
+            customerInfo += address + "\n";
+            customerInfo += phoneNumber + "\n";
+
+            return customerInfo;
+
+        }
+    }
+}
---
+++ Lab 2/SandwichBuilder/SandwichBuilder/Sandwich.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using Microsoft.VisualBasic;
+
+namespace SandwhichBuilder
+{
+    internal class Sandwich
+    {
+        String name;
+
+        String? imagePath; // This needs to be nullable bc not all will have an image
+        String description;
+        String size;
+        String bread;
+        List<String> sauces;
+        List<String> meats;
+        List<String> cheese;
+        List<String>? toppings;
+        List<String>? premToppings;
+
+        public String Name
+        {
+            get { return name; } set { name = value; }
+        }
+        public String? ImagePath //This needs to be nullable
+        {
+            get { return imagePath; } set {imagePath = value;} 
+        }
+
+        public String Description
+        {
+            get { return  description; } set { description = value; }
+        }
+        public String Size
+        {
+            get { return size; } set { size = value;}
+        }
+        public String Bread
+        {
+            get { return bread; } set { bread = value; }
+        }
+        public List<String> sauces
+        {
+            get { return sauces; } set {sauces = value; }
+        }
+        public List<String> meats
+        {
+            get { return meats;} set {meats = value;}
+        }
+        public List<String> Cheese
+        {
+            get { return cheese; } set {cheese = value; }
+        }
+        public List<String>? Toppings
+        {
+            get { return toppings; } set {toppings = value; }
+        }
+        public List<String>? PremToppings // This needs to be nullable since its optional
+        {
+            get { return premToppings; } set {premToppings = value; }
+        }
+        internal Sandwich(String name, String? imagePath, String description,
+        String size,String bread,List<String> sauces,List<String> meats,List<String> cheese,
+        List<String>? toppings,List<String>? premToppings)
+        {
+            this.name = name;
+            this.imagePath = imagePath;
+            this.description = description;
+            this.size = size;
+            this.bread = bread;
+            this.sauces = sauces;
+            this.meats = meats;
+            this.cheese = cheese;
+            this.toppings = toppings;
+            this.premToppings = premToppings;
+
+        }
+        public override String ToString()
+        {
+            String sandwichInfo = "";
+
+            sandwichInfo += name + "\n";
+            sandwichInfo += description + "\n";
+            sandwichInfo += size + "\n";
+            sandwichInfo += bread + "\n";
+
+            // https://stackoverflow.com/questions/759133/how-to-display-list-items-on-console-window-in-c-sharp
+            sandwichInfo += String.Join(", ", sauces) + "\n";
+            sandwichInfo += String.Join(", ", cheese) + "\n";
+            sandwichInfo += String.Join(", ", toppings ?? []) + "\n";
+            sandwichInfo += String.Join(", ", premToppings ?? []);
+
+    return sandwichInfo;
+        }
+    public static class preMadeSandwiches
+        {
+            public static List<Sandwich> preMade = new List<Sandwich>();
+            
+            static preMadeSandwiches (){
+                preMade.Add(new Sandwich("Small love Turkey", null, "Turkey sandwich made with love",
+                "Small", "White", ["Mayo","Ketchup"],["Turkey"],["Cheddar"],["Lettuce"],["Avocado"]));
+
+                preMade.Add(new Sandwich("Large hateful Turkey", null, "Large Turkey sandwich made with hate",
+                "Large", "Rye", ["Hot Sauce"],["Turkey"],["Buffalo Cheese"],["Lettuce"],null));
+
+                preMade.Add(new Sandwich("Family hateful Turkey", null, "Family size Turkey sandwich made with hate",
+                "Family Size", "Long Roll", ["Hot Sauce"],["Turkey"],["Buffalo Cheese"],["Lettuce"],null));
+            }
+            
+        }
+    }
+}
