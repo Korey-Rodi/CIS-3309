cccc87a44c114539c279eddd154ec8b3b44ad3ec
Korey Rodi <rodikorey@gmail.com>
2026-09-16 15:57:12 -0400

    9/16

---
+++ Lab 2/SandwhichBuilder/SandwhichBuilder/Sandwich.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandwhichBuilder
+{
+    internal class Sandwich
+    {
+        String name;
+        String description;
+        String size;
+        String bread;
+        List<String> sauces;
+        List<String> meats;
+        List<String> cheese;
+        List<String> toppings;
+        List<String> premToppings;
+
+        public String Name
+        {
+            get { return name; } set { name = value; }
+        }
+
+        public String Description
+        {
+            get { return  description; } set { description = value; }
+        }
+        public String Size
+        {
+            get { return size; } set { size = value}
+        }
+        internal Sandwich()
+        {
+            name = "";
+            description = "";
+            size = "";
+            sauces = new List<String>();
+            meats = new List<String>();
+            cheese = new List<String>();
+            toppings = new List<String>();
+            premToppings = new List<String>();
+
+        }
+    }
+}
