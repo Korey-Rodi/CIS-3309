025b421cc12056e6b51df9a128ab49c0befebf12
Korey Rodi <rodikorey@gmail.com>
2026-09-17 14:33:53 -0400

    9/17
    Added dicts and cost calculator

--- Lab 2/SandwichBuilder/SandwichBuilder/Pricing.cs
+++ Lab 2/SandwichBuilder/SandwichBuilder/Pricing.cs
@@ -8,7 +8,6 @@ namespace SandwichBuilder
 {
     internal static class Pricing
     {
-
         // Size 5 (small, medium, large, extra-large, and party size)
         // Bread 6 (White, Rye, Long roll, Flatbread, Wheat, Whole Grain)
         // Sauces 8 (Hot sauce, Mayo, Oil, Vinegar, Mustard, Ranch, Ketchup, Chipotle)
@@ -16,6 +15,18 @@ namespace SandwichBuilder
         // Cheese 5 (Cheddar, Pepper Jack, Buffalo Cheese, Gouda, Swiss)
         // Toppings 10 (Lettuce, Tomato, Onion, Pickles, Banana Peppers, Jalapenos, Olives, Cucumbers, Avocado, Spinach)
         // Premium Toppings 10 (Extra Lettuce, Extra Tomato, Extra Onion, Extra Pickles,
+
+        public static Decimal CostCalculator(Dictionary<String, Decimal> Cost, String item)
+        {
+            if (Cost.TryGetValue(item, out Decimal result))
+            {
+                return result;
+            }
+            else
+            {
+                return 0.0m;
+            }
+        }
         internal static Dictionary<String, Decimal> SizePricing = new Dictionary<String, Decimal>
     {
             { "Small", 5.00m },
@@ -28,10 +39,37 @@ namespace SandwichBuilder
     {
             {"White", 1.00m},
             {"Rye",1.00m},
-            {"Long Roll",1.50m }
-            // Add rest
+            {"Long Roll",1.50m },
+            {"Flatbread",.50m },
+            {"Wheat",2.00m},
+            {"Whole Grain",5.00m }
 
     };
+        internal static Dictionary<String, Decimal> saucePricing = new Dictionary<string, decimal>
+        {
+            {"Hot Sauce",.25m},
+            {"Mayo",.10m },
+            {"Oil",.10m },
+            {"Vinegar", .10m},
+            {"Mustard", .10m},
+            {"Ranch", .50m},
+            {"Ketchup",.10m},
+            {"Chipotle", .50m}
+        };
+
+        internal static Dictionary<String, Decimal> meatPricing = new Dictionary<string, decimal>
+        {
+            {"Turkey", 2.00m},
+            {"Ham", 2.00m},
+            {"Roast Beef", 3.50m},
+            {"Chicken", 3.00m},
+            {"Salami",1.00m},
+            {"Pepperoni",1.00m},
+            {"Bacon",1.25m},
+            {"Pastrami",3.00m},
+            {"Capicola", 3.25m},
+            {"Prosciutto",3.75m}
+        };
 
     }
 }
