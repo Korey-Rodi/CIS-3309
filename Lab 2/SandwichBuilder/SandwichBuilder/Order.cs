19c2c6b68307078e3a4734e40cd278a076f99b15
Korey Rodi <rodikorey@gmail.com>
2026-09-21 13:53:52 -0400

    9/21

--- Lab 2/SandwichBuilder/SandwichBuilder/Order.cs
+++ Lab 2/SandwichBuilder/SandwichBuilder/Order.cs
@@ -13,22 +13,24 @@ namespace SandwichBuilder
 
         String orderType;
         Decimal tip;
-        // Decimal subtotal;
+        Decimal subtotal;
 
-        internal Order(Customer customer, Sandwich sandwich, String orderType, Decimal tip)
+        internal Order(Customer customer, Sandwich sandwich, String orderType, Decimal tip,
+        Decimal subtotal)
         {
             this.customer = customer;
             this.sandwich = sandwich;
             this.orderType = orderType;
             this.tip = tip;
-            //this.subtotal = subtotal;
+            this.subtotal = subtotal;
         }
         public override string ToString()
         {
             String orderInfo = "";
 
             orderInfo += customer.ToString() + "\n" + orderType + "\n" +
-            sandwich.ToString();
+            sandwich.ToString() + "\n" + subtotal.ToString("C") + "\n"
+            + tip.ToString("C");
 
             return orderInfo;
 
